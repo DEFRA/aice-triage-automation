b635ab7f8332b47f73456f272288055a8c1d116d
Farhana Abdul Alim <204352779+Farhana-aa@users.noreply.github.com>
2026-07-14 10:17:31 +0100

    add stub scoring engine with offline deterministic output

---
+++ src/agents/engine.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {object} Engine
+ * @property {string} name
+ * @property {(text: string) => Promise<ScoringResult>} score
+ */
+
+/**
+ * @typedef {import('#/domain/scoring-schema.js').ScoringResult} ScoringResult
+ */
